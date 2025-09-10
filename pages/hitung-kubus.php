1d91aa9626c99868f094d2f8d30cc084ecfe4d34
Gif <g2239104@gmail.com>
2025-09-10 10:11:56 +0700

    Add files via upload

---
+++ pages/hitung-kubus.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hitung Kubus</title>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
+<body>
+<!-- ------------------------ Tampilan paling atas ------------------------- -->
+    <div class="header">
+        <p id="header__title">Selamat Datang Di Website Ku</p>
+    </div>
+
+<!-- ---------------- tampilan buat ngrah ke halaman lain ----------------- -->
+    <div class="nav">
+        <h2 id="nav__title">Menu Navigasi</h2>
+        <div class="nav__isi">
+            <a href="../index.html" class="nav__link">Home</a><br>
+            <a href="profile.html" class="nav__link">Profile</a><br>
+            <a href="sosmed.html" class="nav__link">Sosial Media</a><br>
+            <a href="hitung-kubus.php" class="nav__link active">Menghitung Kubus</a><br>
+        </div>
+    </div>
+</body>
+</html>
